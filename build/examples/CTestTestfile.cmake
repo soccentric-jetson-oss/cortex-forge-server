12a8336fa954970e096e1bce0f2ec520c5d0020c
Sandesh Ghimire <sandesh@soccentric.com>
2026-08-05 18:58:50 -0700

    chore: final cleanup and polish

---
+++ build/examples/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sandesh/jetson/build/cortex-forge-server/examples
+# Build directory: /home/sandesh/jetson/build/cortex-forge-server/build/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("basic_client")
---
+++ build/examples/basic_client/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sandesh/jetson/build/cortex-forge-server/examples/basic_client
+# Build directory: /home/sandesh/jetson/build/cortex-forge-server/build/examples/basic_client
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
